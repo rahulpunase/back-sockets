4f4758bdc10763ccef9c9e3ac18ae222c7ccef84
Rahul Punase <rahulpunase@gmail.com>
2019-07-23 15:39:27 +0530

    Revert "Adding login functionality"

--- server/middlewares/logmaker.middleware.js
+++
@@ -1,9 +0,0 @@
-import { logger } from '../middlewares/logger.middleware';
-
-export const logMaker = (req, res, next) => {
-    if(req.originalUrl)
-        logger("ORIGINAL URL:=> " + req.originalUrl);
-    if(req.path)
-        logger("PATH:=> "+req.path);
-    next();    
-}
--- server/routes/authentication.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-import { logMaker } from '../middlewares/logmaker.middleware';
-import { registerUser, loginUser } from '../controllers/authentication.controllers';
-
-router.post("/registeruser", [logMaker], registerUser);
-router.post("/loginuser", [logMaker], loginUser);
-
-export default router;
